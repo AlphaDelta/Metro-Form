2a153742a46748de462f94e76213ee05d88e6fc4
Alfur 'Felt her' Deltur <admin@skid.pro>
2015-06-23 11:21:51 +1200

    Added project

---
+++ Metro/MetroForm.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Metro
+{
+    public class MetroForm : Form
+    {
+        public static class ColorSchema
+        {
+            public static Color
+            cPrimaryLightLight = Color.FromArgb(0x80, 0xCB, 0xEB),
+            cPrimaryLight = Color.FromArgb(0x4F, 0xC8, 0xFC),
+            cPrimary = Color.FromArgb(0x41, 0xB1, 0xE1),
+            cPrimaryDark = Color.FromArgb(0x27, 0x6B, 0x87),
+            cPrimaryDarkDark = Color.FromArgb(0x08, 0x6F, 0x9E),
+
+            cSecondary = Color.FromArgb(0xFF, 0xFF, 0xFF);
+
+            public static SolidBrush
+            bCaption = new SolidBrush(cPrimary),
+            bWindow = new SolidBrush(cSecondary),
+            bCaptionTitle = new SolidBrush(cSecondary),
+            bCaptionControls = new SolidBrush(cPrimaryDark),
+            bCaptionControlsHover = new SolidBrush(cPrimaryLightLight),
+            bCaptionControlsActive = new SolidBrush(cPrimaryDarkDark),
+            bCaptionControlsShadow = new SolidBrush(cPrimaryLight);
+
+            public static Pen
+            pBorder = new Pen(cPrimary),
+            pCaptionControls = new Pen(cPrimaryDark, 2f),
+            pCaptionControlsActive = new Pen(cSecondary, 2f),
+            pCaptionControlsShadow = new Pen(cPrimaryLight, 2f);
+
+            public static void Update()
+            {
+
+            }
+        }
+
+        MetroFormGlow glow = null;
+        public MetroForm()
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.ResizeRedraw | ControlStyles.EnableNotifyMessage, true);
+
+            SetBounds();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+
+            glow = new MetroFormGlow(this);
+            glow.Show();
+            glow.Render();
+            this.Focus();
+        }
+
+        protected override void OnGotFocus(EventArgs e)
+        {
+            base.OnGotFocus(e);
+            glow.Show();
+            glow.Render();
+            
+            //glow.BringToFront();
+            this.BringToFront();
+        }
+
+        protected override void OnLostFocus(EventArgs e)
+        {
+            base.OnLostFocus(e);
+            glow.Hide();
+        }
+
+        Rectangle CaptionBounds, CaptionDragBounds, WindowBounds, btnClose;
+        const int buttonsize = 8, buttonwidth = 34, buttonxmid = (buttonwidth / 2 + buttonsize / 2 + 1);
+        void SetBounds()
+        {
+            CaptionBounds = new Rectangle(0, 0, this.Width, 30);
+            CaptionDragBounds = new Rectangle(0, 0, CaptionBounds.Width - (buttonwidth * capcontrols.Length), CaptionBounds.Height);
+            WindowBounds = new Rectangle(0, 0, this.Width - 1, this.Height - 1);
+
+            btnClose = new Rectangle(CaptionBounds.Width - buttonwidth, 0, buttonwidth, CaptionBounds.Height);
+        }
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            SetBounds();
+            if (glow != null) glow.Render();
+        }
+        protected override void OnLocationChanged(EventArgs e)
+        {
+            base.OnLocationChanged(e);
+
+            if (glow != null) glow.UpdatePosition();
+        }
+
+        Font fCaption = new Font("Microsoft Sans Serif", 10.25f, FontStyle.Regular);
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(ColorSchema.bCaption, CaptionBounds);
+            e.Graphics.DrawRectangle(ColorSchema.pBorder, WindowBounds);
+
+            TextRenderer.DrawText(e.Graphics, this.Text, fCaption, new Rectangle(9, 0, CaptionDragBounds.Width - 9, CaptionBounds.Height), ColorSchema.cSecondary, TextFormatFlags.Left | TextFormatFlags.VerticalCenter | TextFormatFlags.EndEllipsis);
+
+            int midy = (int)Math.Round(CaptionBounds.Height / 2f - 5f);
+            int midys = midy + 1;
+
+            /* Close button */
+            int closex = this.Width - buttonxmid;
+            if (capcontrols[0] == 2)
+            {
+                e.Graphics.FillRectangle(ColorSchema.bCaptionControlsActive, btnClose);
+
+                e.Graphics.DrawLine(ColorSchema.pCaptionControlsActive, closex, midy, closex + buttonsize, midy + buttonsize);
+                e.Graphics.DrawLine(ColorSchema.pCaptionControlsActive, closex, midy + buttonsize, closex + buttonsize, midy);
+            }
+            else
+            {
+                if (capcontrols[0] == 1)
+                    e.Graphics.FillRectangle(ColorSchema.bCaptionControlsHover, btnClose);
+                e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                e.Graphics.DrawLine(ColorSchema.pCaptionControlsShadow, closex, midys, closex + buttonsize, midys + buttonsize);
+                e.Graphics.DrawLine(ColorSchema.pCaptionControlsShadow, closex, midys + buttonsize, closex + buttonsize, midys);
+
+                e.Graphics.DrawLine(ColorSchema.pCaptionControls, closex, midy, closex + buttonsize, midy + buttonsize);
+                e.Graphics.DrawLine(ColorSchema.pCaptionControls, closex, midy + buttonsize, closex + buttonsize, midy);
+            }
+        }
+
+        int[] capcontrols = new int[3];
+        void UpdateCaptionControl(int index, int state)
+        {
+            if (state <= capcontrols[index] && state != 0) return;
+            
+            this.Invalidate();
+            capcontrols[index] = state;
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            e = new MouseEventArgs(e.Button, e.Clicks, e.X - 9, e.Y - 30, e.Delta);
+
+            base.OnMouseMove(e);
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(ColorSchema.bWindow, e.Graphics.ClipBounds);
+            //base.OnPaintBackground(e);
+        }
+
+        const int bordersize = 8;
+        bool nclbtn = false;
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WinAPI.WM_NCHITTEST)
+            {
+                Point mouse = this.PointToClient(Cursor.Position);
+
+                //System.Diagnostics.Debug.WriteLine(mouse.X + ":" + mouse.Y);
+
+                UpdateCaptionControl(0, (btnClose.Contains(mouse) ? (nclbtn ? 2 : 1) : 0));
+
+                foreach (int b in capcontrols)
+                    if (b != 0)
+                    {
+                        m.Result = (IntPtr)WinAPI.HTBORDER;
+                        return;
+                    }
+
+                bool
+                left = false,
+                right = false,
+                top = false,
+                bottom = false;
+
+                if (mouse.X <= bordersize) left = true;
+                else if (mouse.X >= this.Width - bordersize) right = true;
+                if (mouse.Y <= bordersize) top = true;
+                else if (mouse.Y >= this.Height - bordersize) bottom = true;
+
+                if (top && left) m.Result = (IntPtr)WinAPI.HTTOPLEFT;
+                else if (top && right) m.Result = (IntPtr)WinAPI.HTTOPRIGHT;
+                else if (bottom && left) m.Result = (IntPtr)WinAPI.HTBOTTOMLEFT;
+                else if (bottom && right) m.Result = (IntPtr)WinAPI.HTBOTTOMRIGHT;
+                else if (top) m.Result = (IntPtr)WinAPI.HTTOP;
+                else if (bottom) m.Result = (IntPtr)WinAPI.HTBOTTOM;
+                else if (left) m.Result = (IntPtr)WinAPI.HTLEFT;
+                else if (right) m.Result = (IntPtr)WinAPI.HTRIGHT;
+                else if (CaptionDragBounds.Contains(mouse)) m.Result = (IntPtr)WinAPI.HTCAPTION;
+                else if (CaptionBounds.Contains(mouse)) m.Result = (IntPtr)WinAPI.HTBORDER;
+                else m.Result = (IntPtr)WinAPI.HTCLIENT;
+                return;
+            }
+            else if (m.Msg == WinAPI.WM_NCMOUSELEAVE)
+            {
+                nclbtn = false;
+                for (int i = 0; i < capcontrols.Length; i++) capcontrols[i] = 0;
+                this.Invalidate();
+            }
+            else if (m.Msg == WinAPI.WM_NCLBUTTONUP)
+            {
+                nclbtn = false;
+                for (int i = 0; i < capcontrols.Length; i++)
+                    if (capcontrols[i] > 0) capcontrols[i]--;
+                this.Invalidate();
+            }
+            else if (m.Msg == WinAPI.WM_NCLBUTTONDOWN)
+            {
+                nclbtn = true;
+                Point mouse = this.PointToClient(Cursor.Position);
+                UpdateCaptionControl(0, (btnClose.Contains(mouse) ? 2 : 0));
+            }
+            else if (m.Msg == WinAPI.WM_NCCALCSIZE && m.WParam.ToInt32() == 1)
+            {
+                m.Result = new IntPtr(0xF0);
+                return;
+            }
+            else if (m.Msg == WinAPI.WM_SYSCOMMAND)
+            {
+                int command = m.WParam.ToInt32() & 0xFFF0;
+                if (command == WinAPI.SC_MINIMIZE) glow.WindowState = FormWindowState.Minimized;
+                else if (command == WinAPI.SC_RESTORE) glow.WindowState = FormWindowState.Normal;
+            }
+
+            base.WndProc(ref m);
+        }
+    }
+}
---
+++ Metro/MetroFormGlow.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace Metro
+{
+    public class MetroFormGlow : Form
+    {
+        const int glowsize = 10;
+        new MetroForm ParentForm;
+        public MetroFormGlow(MetroForm parent)
+        {
+            ParentForm = parent;
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            //Click through gosh darn it
+            //int initialStyle = WinAPI.GetWindowLong(this.Handle, WinAPI.GWL_EXSTYLE);
+            //WinAPI.SetWindowLong(this.Handle, WinAPI.GWL_EXSTYLE, initialStyle | WinAPI.WS_EX_LAYERED | WinAPI.WS_EX_TRANSPARENT);
+            ShowInTaskbar = false;
+
+            Render();
+        }
+
+        protected override void OnGotFocus(EventArgs e)
+        {
+            ParentForm.BringToFront();
+            base.OnGotFocus(e);
+        }
+
+        public void UpdatePosition() { this.Location = new Point(ParentForm.Location.X - glowsize, ParentForm.Location.Y - glowsize); }
+
+        public void Render()
+        {
+            UpdatePosition();
+
+            Bitmap bitmap = new Bitmap(ParentForm.Width + glowsize * 2, ParentForm.Height + glowsize * 2);
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {//new Rectangle(glowsize, glowsize, ParentForm.Width - glowsize * 2, ParentForm.Height - glowsize * 2)
+                g.FillRectangle(new SolidBrush(Color.FromArgb(0x55, 0x41, 0xB1, 0xE1)), new Rectangle(0, 0, bitmap.Width, bitmap.Height));
+            }
+
+            IntPtr screenDc = WinAPI.GetDC(IntPtr.Zero);
+            IntPtr memDc = WinAPI.CreateCompatibleDC(screenDc);
+            IntPtr hBitmap = IntPtr.Zero;
+            IntPtr oldBitmap = IntPtr.Zero;
+
+            try
+            {
+                hBitmap = bitmap.GetHbitmap(Color.FromArgb(0));  // grab a GDI handle from this GDI+ bitmap
+                oldBitmap = WinAPI.SelectObject(memDc, hBitmap);
+
+                Size size = new Size(bitmap.Width, bitmap.Height);
+                Point pointSource = new Point(0, 0);
+                Point topPos = new Point(Left, Top);
+                WinAPI.BLENDFUNCTION blend = new WinAPI.BLENDFUNCTION();
+                blend.BlendOp = WinAPI.AC_SRC_OVER;
+                blend.BlendFlags = 0;
+                blend.SourceConstantAlpha = 0xFF;
+                blend.AlphaFormat = WinAPI.AC_SRC_ALPHA;
+
+                WinAPI.UpdateLayeredWindow(Handle, screenDc, ref topPos, ref size, memDc, ref pointSource, 0, ref blend, WinAPI.ULW_ALPHA);
+            }
+            finally
+            {
+                WinAPI.ReleaseDC(IntPtr.Zero, screenDc);
+                if (hBitmap != IntPtr.Zero)
+                {
+                    WinAPI.SelectObject(memDc, oldBitmap);
+                    WinAPI.DeleteObject(hBitmap);
+                }
+                WinAPI.DeleteDC(memDc);
+            }
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= WinAPI.WS_EX_LAYERED | WinAPI.WS_EX_TRANSPARENT;
+                return cp;
+            }
+        }
+    }
+}
